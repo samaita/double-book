3da6bc970c5433a0f44ab4ff76e33105e8b6faea
samaita <garysamaita@gmail.com>
2021-10-05 20:34:26 +0700

    Set log to print the file location and it line number

--- app.go
+++ app.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/samaita/double-book/delivery"
 	"github.com/samaita/double-book/repository"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	repository.InitDB("sqlite3", "db_poc.db")
 	delivery.InitHandler()
 }
