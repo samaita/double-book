cdfad08742d81afe7abfc59a5b4cd01f1734e273
samaita <garysamaita@gmail.com>
2021-10-05 18:34:50 +0700

    Remove UNIQUE from DB Schema as AUTOINCREMENT already ensure uniqueness

--- model/db_schema.go
+++ model/db_schema.go
@@ -2,9 +2,9 @@ package model
 
 const (
 	Schema = `
-	CREATE TABLE "user"
+  CREATE TABLE "user"
 	(
-	   "user_id"     INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "user_id"     INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "username"    TEXT UNIQUE,
 	   "display_name"TEXT,
 	   "image_url"   TEXT,
@@ -16,7 +16,7 @@ const (
   
   CREATE TABLE "shop"
 	(
-	   "shop_id"     INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "shop_id"     INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "user_id"     INTEGER,
 	   "name"        TEXT,
 	   "image_url"   TEXT,
@@ -28,7 +28,7 @@ const (
   
   CREATE TABLE "product"
 	(
-	   "product_id"  INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "product_id"  INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "shop_id"     INTEGER,
 	   "name"        TEXT,
 	   "image_url"   TEXT,
@@ -39,7 +39,7 @@ const (
   
   CREATE TABLE "stock"
 	(
-	   "product_id"     INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "product_id"     INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "price_normal"   INTEGER,
 	   "price_discount" INTEGER,
 	   "total"          INTEGER,
@@ -51,7 +51,7 @@ const (
   
   CREATE TABLE "cart"
 	(
-	   "cart_id"     INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "cart_id"     INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "user_id"     INTEGER,
 	   "status"      INTEGER,
 	   "create_time" TEXT,
@@ -60,7 +60,7 @@ const (
   
   CREATE TABLE "cart_detail"
 	(
-	   "cart_detail_id" INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "cart_detail_id" INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "cart_id"        INTEGER,
 	   "product_id"     INTEGER,
 	   "amount"         INTEGER,
@@ -71,7 +71,7 @@ const (
   
   CREATE TABLE "invoice"
 	(
-	   "invoice_id"  INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "invoice_id"  INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "user_id"     INTEGER,
 	   "status"      INTEGER,
 	   "create_time" TEXT,
@@ -80,7 +80,7 @@ const (
   
   CREATE TABLE "invoice_detail"
 	(
-	   "invoice_detail_id" INTEGER PRIMARY KEY UNIQUE AUTOINCREMENT,
+	   "invoice_detail_id" INTEGER PRIMARY KEY AUTOINCREMENT,
 	   "invoice_id"        INTEGER,
 	   "product_id"        INTEGER,
 	   "price_paid"        INTEGER,
