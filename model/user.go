a268174ede08ae487c0a69081fc0ae9d8f7a9f4a
samaita <garysamaita@gmail.com>
2021-10-05 17:49:36 +0700

    Model User now allowed to do query to DB

--- model/user.go
+++ model/user.go
@@ -1,12 +1,21 @@
 package model
 
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/samaita/double-book/repository"
+)
+
 const (
 	userStatusDeactivate = -1
 	userStatusRegistered = 0
 	userStatusActive     = 1
 	userStatusBanned     = 2
 
-	DefaultThumbnail = "https://s4.anilist.co/file/anilistcdn/character/large/b1336-73LQxWKUWy78.png"
+	DefaultUserDisplayName = "User"
+	DefaultUserThumbnail   = "https://s4.anilist.co/file/anilistcdn/character/large/b1336-73LQxWKUWy78.png"
 )
 
 type User struct {
@@ -14,4 +23,34 @@ type User struct {
 	DisplayName string `json:"display_name"`
 	ImageURL    string `json:"image_url"`
 	Status      int    `json:"status"`
+}
+
+func NewUser(id int64) User {
+	return User{
+		UserID:      id,
+		DisplayName: DefaultUserDisplayName,
+		ImageURL:    DefaultUserThumbnail,
+	}
+}
+
+func (u *User) Load() error {
+	var (
+		query string
+		err   error
+	)
+
+	query = `
+		SELECT display_name,  image_url, status
+		FROM user
+		WHERE user_id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	if err = repository.DB.QueryRowContext(ctx, query, u.UserID).Scan(&u.DisplayName, &u.ImageURL, &u.Status); err != nil {
+		log.Printf("[User][Load] Input: %d Output: %v", u.UserID, err)
+		return err
+	}
+
+	return nil
 }
